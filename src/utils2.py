517e6085715d20756b10dd93f3e8cd4f7be57451
Leonard <lseiba@kuberlab.com>
2021-09-03 14:47:10 -0700

    Result comparison

---
+++ src/utils2.py
@@ -0,0 +1,149 @@
+
+from skimage import metrics
+import numpy as np
+import os
+
+import matplotlib.pyplot as plt
+from data import NoisyBSDSDataset
+from argument import Args
+from model import DnCNN, UDnCNN, DUDnCNN, QDUDnCNN
+import nntools as nt
+#from utils import DenoisingStatsManager, plot, NoisyBSDSDataset
+import utils
+import utils2
+from skimage import metrics
+
+import cv2
+import numpy as np
+import torch
+
+import torch.utils.data as td
+import torch.quantization.quantize_fx as quantize_fx
+from torch.quantization.fuse_modules import fuse_known_modules
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as td
+import torchvision as tv
+import snoop
+
+
+# compute the mean squared error and structural similarity
+# index for the images
+def compare_images(imageA, imageB, display=False):
+    # compute the mean squared error and structural similarity
+    # index for the images
+    mse = metrics.mean_squared_error(imageA, imageB)
+    ssim = metrics.structural_similarity(imageA, imageB, multichannel=True)
+    if display:
+        print("Image MSE: %.5f, SSIM: %.5f" % (mse, ssim))
+    return mse,ssim
+
+def myimret(image,):
+    image = image.to('cpu').numpy()
+    image = np.moveaxis(image, [0, 1, 2], [2, 0, 1])
+    image = (image + 1) / 2
+    image[image < 0] = 0
+    image[image > 1] = 1
+    return image
+
+def myimshow(image, ax=plt):
+    image = image.to('cpu').numpy()
+    image = np.moveaxis(image, [0, 1, 2], [2, 0, 1])
+    image = (image + 1) / 2
+    image[image < 0] = 0
+    image[image > 1] = 1
+    h = ax.imshow(image)
+    ax.axis('off')
+    return h
+
+def img_to_tensor(img, device):
+    tensor = torch.FloatTensor(img).to(device)
+    tensor = tensor.permute([2, 0, 1]) / 255.
+    tensor = (tensor - 0.5) / 0.5
+
+    return tensor.unsqueeze(0)
+
+def tensor_to_img(tensor):
+    tensor = tensor[0].permute([1, 2, 0])
+    tensor = (tensor * 0.5 + 0.5) * 255
+    tensor = tensor.clamp(0, 255)
+    return tensor.cpu().numpy().astype(np.uint8)
+
+def quantize_model(quantize_type, model, input_example=None, qat_state=None):
+    if quantize_type == 'dynamic':
+        model = torch.quantization.quantize_dynamic(
+            model,
+            {torch.nn.Conv2d},
+            dtype=torch.qint8
+        )
+    elif quantize_type == 'static':
+        model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+        for i in range(len(model.bn)):
+            conv, bn = model.conv[i+1], model.bn[i]
+            conv_new, bn_new = fuse_known_modules([conv, bn])
+            setattr(model.conv, str(i+1), conv_new)
+            setattr(model.bn, str(i), bn_new)
+        model_fp32_fused = model
+        model_fp32_prepared = torch.quantization.prepare(model_fp32_fused)
+        if input_example is not None:
+            model_fp32_prepared(input_example)
+        model = torch.quantization.convert(model_fp32_prepared)
+    elif quantize_type == 'fx_dynamic':
+        qconfig_dict = {"": torch.quantization.default_dynamic_qconfig}
+        # prepare
+        model_prepared = quantize_fx.prepare_fx(model, qconfig_dict)
+        # no calibration needed when we only have dynamici/weight_only quantization
+        # quantize
+        model = quantize_fx.convert_fx(model_prepared)
+    elif quantize_type == 'fx_static':
+        #qconfig_dict = {"": torch.quantization.get_default_qconfig('qnnpack')}
+        qconfig_dict = {"": torch.quantization.get_default_qconfig('fbgemm')}
+        # prepare
+        model_prepared = quantize_fx.prepare_fx(model, qconfig_dict)
+        # calibrate (not shown)
+        if input_example is not None:
+            model_prepared(input_example)
+        # quantize
+        model = quantize_fx.convert_fx(model_prepared)
+        if qat_state is not None:
+            model.load_state_dict(qat_state)
+
+    return model
+
+
+class NoisyBSDSDataset(td.Dataset):
+    def __init__(self, root_dir, mode='train', image_size=(180, 180), sigma=30):
+        super(NoisyBSDSDataset, self).__init__()
+        self.mode = mode
+        self.image_size = image_size
+        self.sigma = sigma
+        self.images_dir = os.path.join(root_dir, mode)
+        self.files = os.listdir(self.images_dir)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __repr__(self):
+        return "NoisyBSDSDataset(mode={}, image_size={}, sigma={})". \
+            format(self.mode, self.image_size, self.sigma)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.images_dir, self.files[idx])
+        clean = Image.open(img_path).convert('RGB')   
+        # random crop
+        i = np.random.randint(clean.size[0] - self.image_size[0])
+        j = np.random.randint(clean.size[1] - self.image_size[1])
+        
+        clean = clean.crop([i, j, i+self.image_size[0], j+self.image_size[1]])
+        transform = tv.transforms.Compose([
+            # convert it to a tensor
+            tv.transforms.ToTensor(),
+            # normalize it to the range [−1, 1]
+            tv.transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+            ])
+        clean = transform(clean)
+        
+        noisy = clean + 2 / 255 * self.sigma * torch.randn(clean.shape)
+        return noisy, clean
+    
+ 
